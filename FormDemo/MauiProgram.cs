38111cb356a1daf83ecc7a99a7ba1c10ebc471b3
PecsCorreia <verycorreia23@gmail.com>
2023-02-05 22:45:03 -0100

    add ConfigureSyncfusionCore

--- FormDemo/MauiProgram.cs
+++ FormDemo/MauiProgram.cs
@@ -12,7 +12,7 @@ public static class MauiProgram
 		builder
 			.UseMauiApp<App>()
 			.UseMauiCommunityToolkit()
-            .ConfigureSyncfusionCore() 
+            .ConfigureSyncfusionCore()  
             .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
